e06fa7a840460af5c87ad0868e21c63f6619744e
MirQ-hub <n2vami00@students.oamk.fi>
2023-10-03 11:51:13 +0300

    kansioita siivottu

---
+++ Luentomateriaalit/PlotlyExample.py
@@ -0,0 +1,24 @@
+#pip install dash
+from dash import Dash, html, dcc, Input, Output, callback
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash(__name__) #new Dash app named "app"
+
+app.layout = html.Div([
+    html.H1(children='Hello Dash', style={'textAlign': 'center'}),
+    dcc.Dropdown(df.country.unique(), 'Finland', id='dropdown-country'),
+    dcc.Graph(id='graph-content')
+])
+
+@callback(Output('graph-content', 'figure'), 
+          Input('dropdown-country', 'value'))
+
+def update_graph(value):
+    dff = df[df.country == value]
+    return px.line(dff, x='year', y='pop', color='country')
+
+if __name__ == '__main__':
+    app.run_server(debug=False, use_reloader=False)
---
+++ streamlit/mirq_home.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
+st.write("Hello world")
+
+st.header('Data')
+st.header('A header with _italics_ :blue[colors] and **bold** text and emoji! :sunglasses:')
+
+st.markdown('Streamlit can use **_use_ markdowns**')
+st.markdown('This text is :red[colored red], and this is **:blue[colored blue]** and bold')
+
+st.markdown(':green[$\sqrt{x^2+y^2}=1$] is Pythagoras theorem. :smile:')
+
+st.write('Adding widget is same as declaring variable, no need for backends, routes, request etc.')
+
+
+file = st.file_uploader('Upload file in CSV or Excel format', type=['csv', 'xlsx'])
+
+if file is not None:
+    # try:
+    #     df = pd.read_csv(file)
+    #     st.header(file.name)
+    #     st.dataframe(df)
+    # except:
+    #     df = pd.read_excel(file)
+    #     st.header(file.name)
+    #     st.dataframe(df)
+
+    # try:
+    #     df2 = pd.DataFrame(df.groupby('STATION').mean())    
+    #     st.map(df2)
+    # except:
+    #     pass
+
+
+    try:
+        df = pd.read_csv(file, parse_dates=['Time'])
+
+        start_date, end_date = st.select_slider(
+        'Select a time range',
+        options=list(df['Time']),
+        value=(df['Time'].min(), df['Time'].max()))
+
+        mask = (df['Time'] > start_date) & (df['Time'] <= end_date)
+
+        st.line_chart(df.loc[mask], x="Time", y="Price")
+    except Exception as e:
+        st.write(e)
+
+
+
+
+
